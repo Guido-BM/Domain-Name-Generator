97adf1a6c6c3388f547b329d868deac45d3fab0b
loveradrian28 <adrianelovera@gmail.com>
2023-10-10 21:37:38 +0200

    cambios DNG

--- DNG.js
+++ DNG.js
@@ -1,3 +1,11 @@
-const part1 = ["my", "the", "your"];
-const part2 = ["great", "super", "mega", "really"];
-const part3 = ["website", "project", "app"];
+const part1 = ["my", "the"];
+const part2 = ["great", "super"];
+const part3 = ["website", "project"];
+
+for(let i = 0; i < part1.length; i++){
+    for(let j = 0; j < part2.length; j++) {
+        for (let k = 0; k < part3.length; k++){
+            console.log(`${part1[i]}${part2[j]}${part3[k]}.com`)
+        }
+    }
+}
