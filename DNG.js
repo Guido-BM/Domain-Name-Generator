4bedd0cfe3f0e07a0dac932173978570a5009543
Guido <guidoboninomuelas@gmail.com>
2023-10-10 21:26:01 +0200

    funcion con for>for>for

--- DNG.js
+++ DNG.js
@@ -1,3 +1,24 @@
-const part1 = ["my", "the", "your"];
-const part2 = ["great", "super", "mega", "really"];
-const part3 = ["website", "project", "app"];
+const part1 = ["my", "the"];
+const part2 = ["great", "super"];
+const part3 = ["website", "project"];
+
+const input = input;
+
+const generateDomainNames = () => {
+  const domainNames = [];
+
+  for (const p1 of part1) {
+    for (const p2 of part2) {
+      for (const p3 of part3) {
+        const domainName = `${p1}${p2}${input}${p3}.com`;
+        domainNames.push(domainName);
+      }
+    }
+  }
+
+  return domainNames;
+};
+
+const domainNameList = generateDomainNames();
+
+console.log(domainNameList);
