1683f22b82bb701771c98dab76edde782c5858bb
Guido <guidoboninomuelas@gmail.com>
2023-10-11 09:17:24 +0200

    Interactivo

--- DNG.js
+++ DNG.js
@@ -2,23 +2,25 @@ const part1 = ["my", "the"];
 const part2 = ["great", "super"];
 const part3 = ["website", "project"];
 
-const input = input;
+const inputElement = document.getElementById("input");
+const domainList = document.getElementById("domainList");
+const generateAndDisplayDomainNames = () => {
+  const input = inputElement.value;
 
-const generateDomainNames = () => {
-  const domainNames = [];
+  domainList.innerHTML = "";
+
+  let domainNames = [];
 
   for (const p1 of part1) {
     for (const p2 of part2) {
       for (const p3 of part3) {
         const domainName = `${p1}${p2}${input}${p3}.com`;
         domainNames.push(domainName);
+
+        const listItem = document.createElement("li");
+        listItem.textContent = domainName;
+        domainList.appendChild(listItem);
       }
     }
   }
-
-  return domainNames;
 };
-
-const domainNameList = generateDomainNames();
-
-console.log(domainNameList);
